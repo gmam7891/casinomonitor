f3c987587f9c4294a080d77070de02cdd885d171
gmam7891 <guilhermemontanari@gmail.com>
2025-04-24 18:38:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -394,7 +394,6 @@ with st.sidebar.expander("🎯 Análise de VOD / Período"):
                     st,
                     st.session_state,
                     prever_jogo_em_frame,
-                    varrer_url_customizada_paralela,
                     obter_url_m3u8_twitch
                 )
 
