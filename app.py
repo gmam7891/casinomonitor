d02275c97c614cb2f83207974a9ae176603386a4
gmam7891 <guilhermemontanari@gmail.com>
2025-04-24 12:56:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -207,7 +207,23 @@ def processar_frame(m3u8_url, tempo, modelo, nome_jogo_previsto=None):
         print(f"[ERRO] Frame não capturado no segundo {tempo}")
     return None
 
-    def varrer_url_customizada_paralela
+    def varrer_url_customizada_paralela(m3u8_url, st, session_state, prever_jogo_fn, skip_inicial=0, intervalo=5, max_frames=2160):
+    resultados = []
+    tempos = [skip_inicial + i * intervalo for i in range(max_frames)]
+
+    with ThreadPoolExecutor(max_workers=4) as executor:
+        futures = [
+            executor.submit(processar_frame, m3u8_url, tempo, session_state["modelo_ml"])
+            for tempo in tempos
+        ]
+        for future in futures:
+            res = future.result()
+            if res:
+                resultados.append(res)
+
+    session_state["dados_url"] = resultados
+    return resultados
+
 
     tempos = [skip_inicial + i * intervalo for i in range(max_frames)]
 
