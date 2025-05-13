69f74642c24867034f35e8281447ad5534df9dba
gmam7891 <guilhermemontanari@gmail.com>
2025-05-13 16:32:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -552,6 +552,7 @@ if 'dados_url' in st.session_state:
 
 
 # ------------------ BOTÕES PRINCIPAIS ------------------
+import plotly.express as px
 col1, col2, col3, col4 = st.columns(4)
 
 with col1:
